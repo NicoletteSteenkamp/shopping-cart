a8aab765cefdd66320332b7b514494c244569605
Nicolette <steenkampnicolette815@gmail.com>
2023-10-22 00:34:15 +0200

    created,styled and completed app
    added HTML,CSS and JS

---
+++ data.js
@@ -0,0 +1,60 @@
+let shop = document.getElementById("shop");
+
+let shopItemData = [
+  {
+    id: "itigds",
+    name: "SuperSkin Serum",
+    price: 850,
+    desc: "Packed with essential nutrients and vitamins .",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "gjdks",
+    name: "SuperSkin Range",
+    price: 2850,
+    desc: "Discover a new level of beauty and confidence.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "liok",
+    name: "SuperSkin Cleanser",
+    price: 450,
+    desc: "Made with the very best for a gentle but deepclean",
+    img: "images/img-4.jpg",
+  },
+  {
+    id: "glfs",
+    name: "SuperSkin PinkPower",
+    price: 3850,
+    desc: "Our pink range is for our Pink lovers",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "jfhgbvnscs",
+    name: "SuperSkin I-serum",
+    price: 690,
+    desc: "Rargets all 7 layers of the skin,leaving you crow-free ",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "SuperSkin Toner",
+    price: 200,
+    desc: "That extra snatched vibes from the best toner",
+    img: "images/img-7.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "SuperSkin Moisture",
+    price: 320,
+    desc: "Nothing else like it, light-weight and powerful",
+    img: "images/img-6.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "SuperSkin BodyOil",
+    price: 300,
+    desc: "Your skin will thank you for this one",
+    img: "images/img-8.jpg",
+  },
+];
